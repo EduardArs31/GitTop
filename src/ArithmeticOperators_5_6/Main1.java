1a2da75efca4027b453561948e930422f4ceb82a
Eduard <179833121+EduardArs31@users.noreply.github.com>
2024-09-15 18:30:16 +0500

    homework 5-6

---
+++ src/ArithmeticOperators_5_6/Main1.java
@@ -0,0 +1,26 @@
+package ArithmeticOperators_5_6;
+
+public class Main1 {
+    public static void main( String [] args ){
+    //arithmetic_operations
+
+    int a = 100_000;
+    int b = 50_000;
+
+    int x1 = a + b;
+    int x2 = a - b;
+    int x3 = a * b;
+    int x4 = a / b;
+    int x5 = a % b;
+        System.out.println( x1+x2+x3+x4+x5 );
+        System.out.println( x1*x2-x3/x4+x5 );
+        System.out.println();
+        System.out.println();
+        System.out.println( - x1 );
+        System.out.println( - x2 );
+        System.out.println( - x3 );
+        System.out.println( - x4 );
+        System.out.println( - x5 );
+    }
+}
+
