7a1dc8eeda7df5bd4c5e128a709d79cd9b6a1143
Eduard <179833121+EduardArs31@users.noreply.github.com>
2024-09-15 18:36:04 +0500

    homework 9-10

---
+++ src/Branching_operators_9_10/Main.java
@@ -0,0 +1,43 @@
+package Branching_operators_9_10;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 100;
+
+        if (num1 <= num2) {
+            System.out.println("вскипятить чайник");
+        }
+        if (num1 <= num2) {
+            System.out.println("открыть полку взять банку с кофе");
+
+        }
+        if (num2 <= num2) {
+            System.out.println("есть ли у нас кофе?");
+
+        }
+        if (num1 == num2) {
+            System.out.println("берем кружку");
+        } else if (num1 < num2) {
+            System.out.println("собираемся в магазин за кофе");
+        }
+        if (num1 < num2) {
+            System.out.println("купили кофе идем домой");
+        }
+        if (num1 == num2) {
+            System.out.println("добавляем сахар");
+        }
+        if (num1 < num2) {
+            System.out.println("берем кружку");
+        }
+        if (num1 < num2) {
+            System.out.println("добавляем сахар");
+        }
+        if (num1 <= num2) {
+            System.out.println("завариваем кофе");
+        }
+        if (num1 <= num2) {
+            System.out.println("пьем кофе");
+        }
+    }
+}
